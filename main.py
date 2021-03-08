20d979f5f7b840aa3e6924f0dbe86d32ebecf543
Connor Ajersch <ajersch@uwindsor.ca>
2021-03-08 13:12:12 -0500

    files added

---
+++ main.py
@@ -0,0 +1,20 @@
+# import the necessary packages
+from scipy.spatial import distance as dist
+from imutils.video import FileVideoStream
+from imutils.video import VideoStream
+from imutils import face_utils
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+import time
+import csv
+
+
+def main():
+    raise NotImplemented()
+
+
+if __name__ == '__main__':
+    main()
