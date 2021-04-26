7bcc6b8e2dd5928c2882e4685e3752d5d538edac
mathewdunne <mathewdd3@gmail.com>
2021-04-26 15:08:56 -0400

    Added slider, still needs to be connected to blink detector

--- control_gui.py
+++ control_gui.py
@@ -2,6 +2,7 @@ import time
 
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QSlider
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import QHBoxLayout
@@ -12,10 +13,9 @@ from PyQt5.QtCore import Qt
 import detect_blinks
 
 class MainWidget(QWidget):
-    """
-    Widget for admin related tasks
-    """
+
     isCollectingData = False
+    defaultThreshold = 0.27
 
     def __init__(self, disk_dir: str):
         super(MainWidget, self).__init__()
@@ -48,8 +48,40 @@ class MainWidget(QWidget):
         main_button_widget.setLayout(start_stop_button_layout)
 
 
+        #slider to set threshold
+        threshold_slider_layout = QHBoxLayout()
+        threshold_slider_layout.setAlignment(Qt.AlignHCenter)
+        self.thresholdSlider = QSlider(Qt.Horizontal, self)
+        self.thresholdSlider.setRange(0,100)
+        self.thresholdSlider.setFocusPolicy(Qt.NoFocus)
+        self.thresholdSlider.setPageStep(10)
+        self.thresholdSlider.setSingleStep(1)
+        self.thresholdSlider.setValue(50)
+
+        #label of threshold value
+        self.thresholdLabel = QLabel(str(self.defaultThreshold), self)
+        self.thresholdLabel.setAlignment(Qt.AlignHCenter)
+        self.thresholdLabel.setMaximumWidth(80)
+
+        #other label
+        self.sliderTitleLabel = QLabel("Threshold Value for Blink Detection (Default = " + str(self.defaultThreshold) + "):")
+
+        threshold_slider_layout.addWidget(self.thresholdSlider)
+        threshold_slider_layout.addSpacing(15)
+        threshold_slider_layout.addWidget(self.thresholdLabel)
+
+        threshold_slider_widget = QWidget()
+        threshold_slider_widget.setLayout(threshold_slider_layout)
+
+        vertical_slider_layout = QVBoxLayout()
+        vertical_slider_layout.addWidget(self.sliderTitleLabel)
+        vertical_slider_layout.addWidget(threshold_slider_widget)
+        vertical_slider_widget = QWidget()
+        vertical_slider_widget.setLayout(vertical_slider_layout)
+
         layout = QVBoxLayout()
         layout.addWidget(main_button_widget)
+        layout.addWidget(vertical_slider_widget)
 
         layout.setAlignment(Qt.AlignTop)
         self.setLayout(layout)
@@ -65,6 +97,7 @@ class MainWidget(QWidget):
         Sets up all of the connections between the components of the Widget
         """
         self.start_stop_button.pressed.connect(self.toggleDataCollection)
+        self.thresholdSlider.valueChanged.connect(self.sliderValueChanged)
 
     def toggleDataCollection(self):
         """
@@ -85,3 +118,10 @@ class MainWidget(QWidget):
             self.start_stop_button.setStyleSheet("background-color: #f58997")
             time.sleep(3)
             self.isCollectingData = True
+
+    def sliderValueChanged(self, value):
+
+        #convert value from int (0-100) to float
+        realValue = (0.27/50)*value
+
+        self.thresholdLabel.setText(str(realValue.__round__(2)))
